2535a0e52999131ae4becd471ef9fbfb72d170f8
123321abccba1 <heye@2002heye.vip>
2026-01-17 20:00:31 +0000

    done heuristics

--- A1 Files/heuristics.py
+++ A1 Files/heuristics.py
@@ -38,10 +38,44 @@ var_ordering == a function with the following template
 
 def ord_dh(csp):
     ''' return next Variable to be assigned according to the Degree Heuristic '''
-    # IMPLEMENT
-    pass
+    unassigned = csp.get_all_unasgn_vars()
+    if not unassigned:
+        return None
+
+    best_var = None
+    best_degree = -1
+
+    unassigned_set = set(unassigned)
+
+    for v in unassigned:
+        degree = 0
+        for con in csp.get_cons_with_var(v):
+            # count how many OTHER unassigned vars are in this constraint's scope
+            for u in con.get_scope():
+                if u is not v and u in unassigned_set:
+                    degree += 1
+
+        if degree > best_degree:
+            best_degree = degree
+            best_var = v
+
+    return best_var
+
 
 def ord_mrv(csp):
     ''' return Variable to be assigned according to the Minimum Remaining Values heuristic '''
-    # IMPLEMENT
-    pass
+    unassigned = csp.get_all_unasgn_vars()
+    if not unassigned:
+        return None
+
+    # pick the variable with the smallest current domain size
+    mrv_var = unassigned[0]
+    min_size = mrv_var.cur_domain_size()
+
+    for v in unassigned[1:]:
+        sz = v.cur_domain_size()
+        if sz < min_size:
+            min_size = sz
+            mrv_var = v
+
+    return mrv_var
